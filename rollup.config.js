7d38c8cd83e775a3abdc15cee7fd681bc76b0300
Md Faizaan <aulisius7@gmail.com>
2018-05-28 11:36:21 +0530

    Improve packaging and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,64 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import copy from "rollup-plugin-copy-assets";
+import { uglify } from "rollup-plugin-uglify";
+import { minify } from "uglify-es";
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.js",
-  external: Object.keys(pkg.peerDependencies),
-  output: [{ file: "lib/cjs/side-effect.js", format: "cjs" }],
-  plugins: [
+const INPUT = "src/index.js";
+const peerDependencies = Object.keys(pkg.peerDependencies);
+const createBuild = ({ input, filename, format, external, plugins }) => ({
+  input,
+  external,
+  output: {
+    file: filename,
+    format
+  },
+  plugins
+});
+
+const createCjsBuild = ({ production = false }) => {
+  let plugins = [
+    babel({
+      exclude: "node_modules/**"
+    }),
+    commonjs()
+  ];
+  if (production) {
+    plugins.push(uglify({}, minify));
+  }
+  let filename = `lib/cjs/side-effect${production ? ".min" : ""}.js`;
+  return createBuild({
+    input: INPUT,
+    filename,
+    format: "cjs",
+    external: peerDependencies,
+    plugins
+  });
+};
+
+const createESBuild = _ => {
+  let plugins = [
     babel({
       exclude: "node_modules/**"
     }),
     commonjs(),
     copy({
-      assets: ["./index.js"]
+      assets: ["./index.cjs.js", "./index.es.js"]
     })
-  ]
+  ];
+  let filename = "lib/es/side-effect.js";
+  return createBuild({
+    input: INPUT,
+    filename,
+    format: "es",
+    external: peerDependencies,
+    plugins
+  });
 };
+
+export default [
+  createCjsBuild({ production: false }),
+  createCjsBuild({ production: true }),
+  createESBuild()
+];
