45edc6353a9c55a6afb0dc6ad758d3e03564687a
aulisius <aulisius7@gmail.com>
2020-03-09 16:01:05 +0530

    Update configuration

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ import babel from "rollup-plugin-babel";
 import filesize from "rollup-plugin-filesize";
 import pkg from "./package.json";
 
-let createBuild = ({ format }) => ({
+let createBuild = format => ({
   input: pkg.source,
   external: Object.keys(pkg.peerDependencies),
   output: { file: `lib/${format}.js`, format },
   plugins: [babel({ exclude: "node_modules/**" }), filesize()]
 });
 
-export default [createBuild({ format: "cjs" }), createBuild({ format: "es" })];
+export default [createBuild("cjs"), createBuild("esm")];
